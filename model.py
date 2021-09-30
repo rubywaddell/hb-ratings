f4918362079ad9d0c7f1acb8ced3952d44cf9fd7
katalinschmidt <katalin.schmidt.97@gmail.com>
2021-09-29 17:56:16 -0700

    Complete classes and tables

--- model.py
+++ model.py
@@ -7,17 +7,53 @@ db = SQLAlchemy()
 class User(db.Model):
     """Movie app user"""
 
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
     user_email = db.Column(db.String, unique = True, nullable = False)
     user_password = db.Column(db.String, unique = False, nullable = False)
 
+    # ratings = a list of Rating objects (product of backref in Rating class)
+
     def __repr__(self):
         return f"<User user_id={self.user_id} email={self.user_email} password={self.user_password}>"
 
 
-def connect_to_db(flask_app, db_uri="postgresql:///ratings", echo=True):
+class Movie(db.Model):
+    """Movies in app"""
+
+    __tablename__ = "movies"
+
+    movie_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
+    movie_title = db.Column(db.String)
+    movie_overview = db.Column(db.Text)
+    movie_release_date = db.Column(db.DateTime)
+    movie_poster_path = db.Column(db.String)
+
+    # ratings = a list of Rating objects (product of backref in Rating class)
+
+    def __repr__(self):
+        return f"<Movie movie_id={self.movie_id} movie_title={self.movie_title}>"
+
+
+class Rating(db.Model):
+    """Ratings of movies in app"""
+
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement = True, primary_key = True) 
+    score = db.Column(db.Integer)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    movie = db.relationship("Movie", backref="ratings")
+    user = db.relationship("User", backref="ratings")
+
+    def __repr__(self):
+        return f"<Movie Rating rating_id={self.rating_id} score={self.score}>"
+
+
+def connect_to_db(flask_app, db_uri="postgresql:///ratings_lab", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
