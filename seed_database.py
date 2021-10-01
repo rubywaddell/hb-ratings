eef18956cb1a9f9af03ddbdb1223e8b9a96fcb5e
katalinschmidt <katalin.schmidt.97@gmail.com>
2021-09-30 17:24:52 -0700

    Seed movies

--- seed_database.py
+++ seed_database.py
@@ -8,3 +8,23 @@ import server
 import model
 import crud
 
+os.system('dropdb ratings')
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/movies.json') as f:
+    movie_data = json.loads(f.read())
+
+movies_in_db = []
+for movie in movie_data:
+    formatted_release_date = datetime.strptime(movie["release_date"], "%Y-%m-%d")
+
+    db_movie = crud.create_movie(
+                    movie['title'], 
+                    movie['overview'], 
+                    formatted_release_date, 
+                    movie['poster_path'])
+                    
+    movies_in_db.append(db_movie)
